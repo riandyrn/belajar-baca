b21f4544aae92ed0f8b7643232e3f9221df5896f
Riandy Rahman Nugraha <riandyrn@gmail.com>
2020-06-07 11:27:22 +0300

    implement layout; work on progress;

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {},
+    methods: {}
+});
+
+function changeImage() {
+    alert("changing image...")
+}
+
+function readCaption() {
+    alert("reading caption...")
+}
